7c946674b9465b214320d284a235805fecae37e1
Xander Jones <xander@bugsnag.com>
2021-02-02 20:17:42 +0000

    A better example

--- ptm_test.py
+++ ptm_test.py
@@ -1,24 +1,30 @@
 '''
     Script:      ptm_test.py
-    Description: Python pretty tree maker test harness
+    Description: Python pretty tree maker example
     Author:      Xander Jones (xander@xljones.com)
     Web:         xljones.com
-    Date:        12 May 2020
+    Date:        2 Feb 2021
 '''
 
+# import the lib
 import ptm
 
-# create a tree object
+# create a new tree
 myTree = ptm.Tree()
 
-# add an element to the tree
-myTree.add_element("root1")
-myTree.add_branch("bra1")
-myTree.add_element("el1", "bra1")
-myTree.add_element("el2", "bra1")
-myTree.add_branch("bra2")
-myTree.add_branch("bra3")
-myTree.add_element("el3", "bra3")
+# adding a branch on the tree root
+myTree.add_branch("Recipe")
 
-myTree.print_tree()
+# adding a branch onto the branch "Recipe"
+myTree.add_branch("Ingredients", "Recipe")
+ingredients = ["100g plain flour", "2 large eggs", "300ml milk", "butter", "lemon", "caster sugar"]
+for ingredient in ingredients:
+    myTree.add_element(ingredient, "Recipe.Ingredients")
 
+myTree.add_branch("Instructions", "Recipe")
+myTree.add_element("It's a secret", "Recipe.Instructions")
+
+myTree.add_branch("Results", "Recipe")
+myTree.add_element("A mess", "Recipe.Results")
+
+myTree.print()
